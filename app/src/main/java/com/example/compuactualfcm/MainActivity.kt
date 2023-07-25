1611609f51bec525a6c43d87c0f3ac7077af356e
Jose Armando Salazar Moreno <samja1701ap@gmail.com>
2023-07-25 02:33:58 -0600

    Initial commit

---
+++ app/src/main/java/com/example/compuactualfcm/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.compuactualfcm
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.tvRegister)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "¡Bienvenido!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MenuActiviy::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/compuactualfcm/MenuActiviy.kt
@@ -0,0 +1,13 @@
+package com.example.compuactualfcm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActiviy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_activiy)
+
+
+    }
+}
