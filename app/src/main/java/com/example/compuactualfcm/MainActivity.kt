9e25cf1dffc37e6ee4346742c64d63a8b7bf2041
Jose Armando Salazar Moreno <samja1701ap@gmail.com>
2023-07-30 01:25:12 -0600

    Snackbar

--- app/src/main/java/com/example/compuactualfcm/MainActivity.kt
+++ app/src/main/java/com/example/compuactualfcm/MainActivity.kt
@@ -3,15 +3,25 @@ package com.example.compuactualfcm
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.compuactualfcm.PreferenceHelper.get
 import com.example.compuactualfcm.PreferenceHelper.set
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    val lay: LinearLayout by lazy { findViewById(R.id.mainlayout) }
+    private val snackBar by lazy {
+        Snackbar.make(lay,R.string.press_back_again,Snackbar.LENGTH_SHORT)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         //extraer id del xml
         val button = findViewById<Button>(R.id.tvRegister)
@@ -50,4 +60,15 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
         finish()
     }
+
+    override fun onBackPressed() {
+       if(snackBar.isShown)
+        super.onBackPressed()
+        else
+            snackBar.show()
+    }
 }
+
+
+
+
